da352db369bbd970e777dd1dcaa169ca7ccfd988
Hidenori Suzuki <hidsuzuk@HidenorinoMacBook-puro.local>
2018-08-23 01:54:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# wetrans
+
+    This is a command line tool to use on terminal.
+    When you specify Japanese, it translate it into English.
+    When you specify English, it translate it into Japanese.
+
+# settings
+
+    pip install googletrans
+    git clone git@github.com:hidsuzuk/wetrans.git
+    cd wetrans
+
+# usage
+
+    ./wetrans.py
+    > ランチに行きましょう 
+    Let's go to lunch
+    > Who have not gotten PayPay T-shirts yet? 
+    PayPayのTシャツをまだ手に入れていない人はいますか？
---
+++ wetrans.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import unicodedata
+from subprocess import getoutput
+from googletrans import Translator
+
+def is_japanese(string):
+    for ch in string:
+        name = unicodedata.name(ch) 
+        if "CJK UNIFIED" in name \
+        or "HIRAGANA" in name \
+        or "KATAKANA" in name:
+            return True
+    return False
+
+def translate(data):
+    trans = Translator()
+    if japanese:
+        output = trans.translate(data, dest='en').text
+    else:
+        output = trans.translate(data, dest='ja').text
+    return output
+
+if __name__ == '__main__':
+    while True:
+        data = input("> ")
+        japanese = is_japanese(data)
+        print(translate(data))
